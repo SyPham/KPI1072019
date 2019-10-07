f48ae1513d6ffb1d63f0e6de72d72c21d7e31b29
Sỹ Phạm <phamtiensy8696@gmail.com>
2019-10-07 16:25:43 +0700

    config route

---
+++ KPI.Web/Scripts/app/main/components/HomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+       This is a index page
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ KPI.Web/Scripts/app/main/components/admin/categoryKPILevel/CategoryKPILevelComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Category KPILevel Components</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
